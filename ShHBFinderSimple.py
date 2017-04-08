b269d510ca557fb3e55f947d3c6ea0f39e4b649a
Ld105 <luis.duarte.843@gmail.com>
2017-04-08 17:53:43 +0100

    Add files via upload

---
+++ ShHBFinderSimple.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+#Hearthbleed Finder Simple-Simplezao mesmo Beta 1.0
+#By Ld105
+#Salve pro v0ldin-yunkers-tio uli
+#Achem suas Keys de API no site da shodan
+
+import sys
+import re
+import socket
+import shodan
+
+SHODAN_API_KEY = (Key)
+
+try:
+api = shodan.shodan(key)
+
+ print "[Ld105]Finding Hearthbleeds,wait fucking hacker pervert[Ld105]"
+
+     def results           
+          try:
+        results = api.search(vuln:cve-2014-0160 )
+		api.streams.ports([443, 8443])
+ 
+print ' U found some vuln sites but u not found a social life '
+print  'Hearthbleed Vulnerable sites Found: %s' % results['all']
+
+if  results['all'] = 0:
+"U foundnt vulnerable sites too :C"
+sys.exit()
+ else results['all'] >= 1:
+ print 'Adrr %s' % result['ip_str']
+ 
+        
